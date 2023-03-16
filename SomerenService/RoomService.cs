79c3727bb62da1f66a9b4e4eab6fba5ba5b2ffee
Ahasanul Rabbi Khan <me.ahsanul01@gmail.com>
2023-03-16 19:08:20 +0100

    lecturer update

---
+++ SomerenService/RoomService.cs
@@ -0,0 +1,26 @@
+﻿using SomerenDAL;
+using SomerenModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomerenService
+{
+    public class RoomService
+    {
+        private RoomDao roomDb;
+
+        public RoomService()
+        {
+            roomDb = new RoomDao();
+        }
+
+        public List<Room> GetTeachers()
+        {
+            List<Room> rooms = roomDb.GetAllRooms();
+            return rooms;
+        }
+    }
+}
